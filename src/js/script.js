103f0c7620e227023c0ef721479f12ed6ee3982a
gabriel <gabriel.ferreira.machado74@gmail.com>
2025-04-01 09:10:20 -0300

    declaraçao de variavel

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+//declaraçoes e variaveis
+var nome ="fiap";
+console.log(nome)
+
+let idade ="17";
+console.log(idade)
+const sobrenome ="cidade";
+console.log(sobrenome)
+
+let aula;
+console.log(aula)
+let valor= null;
+console.log(valor)
+let exemplo1={};
+
+let exemplo2=[];
